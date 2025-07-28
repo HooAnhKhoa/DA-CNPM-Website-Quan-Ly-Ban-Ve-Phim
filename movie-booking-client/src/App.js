9cf63cdeca99d1b570122ce7bd71999447e80d05
HooAnhKhoa <anhkhoa12204@gmail.com>
2025-07-28 10:26:58 +0700

    Thêm nội dung app.js(Điều hướng) và index.js

--- movie-booking-client/src/App.js
+++ movie-booking-client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import NowShowingPage from './pages/NowShowingPage';
+import ComingSoonPage from './pages/ComingSoonPage';
+import AddMoviePage from './pages/AddMoviePage';
+import MovieDetailPage from './pages/MovieDetailPage';
+import BookingPage from './pages/BookingPage';
+import MoviePage from './pages/MoviePage';
+import BookedTickets from './pages/BookedTickets';
+import AddShowtimePage from './pages/AddShowtimePage';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/api';
+
+export const getMovies = () => axios.get(`${API_URL}/movies`);
+export const addMovie = (movie) => axios.post(`${API_URL}/movies`, movie);
+// Thêm các hàm khác nếu cần
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviePage />} />
+        <Route path="/movies/now-showing" element={<NowShowingPage />} />
+        <Route path="/movies/coming-soon" element={<ComingSoonPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/add-movie" element={<AddMoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/booked-tickets" element={<BookedTickets />} />
+        <Route path="/add-showtime" element={<AddShowtimePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
